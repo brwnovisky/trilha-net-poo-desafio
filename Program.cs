93689eb2a3e279d9ebc0498a0f89a52fde6597af
brwnovisky <brwnovisky@hotmail.com>
2024-08-30 14:18:19 -0300

    Solucao ver 1.0

--- Program.cs
+++ Program.cs
@@ -1,3 +1,28 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = 
+    new Nokia
+        (
+            numero: "+55999876543", 
+            modelo: "X30", 
+            imei: "543523453454", 
+            memoria: 128
+        );
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = 
+    new Iphone
+    (
+        numero: "+5501923645789", 
+        modelo: "15 Pro Max", 
+        imei: "543523453454", 
+        memoria: 256
+    );
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
